1e00ca01e7dad9a039000f74145df989b4440f6e
bhavika1 <bhavika.isha@gmail.com>
2021-03-07 23:27:09 +0530

    Feature 14 - Using refresh token in angular

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -2,9 +2,11 @@ import { NgModule } from '@angular/core';
 import { Routes, RouterModule } from '@angular/router';
 import { LoginComponent } from './auth/login/login.component';
 import { SignupComponent } from './auth/signup/signup.component';
+import { HomeComponent } from './home/home.component';
 
 
 const routes: Routes = [
+  { path: '', component: HomeComponent},
   { path: 'signup', component: SignupComponent },
   { path: 'login', component: LoginComponent}
 ];
--- src/app/auth/signup/signup.component.ts
+++ src/app/auth/signup/signup.component.ts
@@ -38,16 +38,11 @@ export class SignupComponent implements OnInit {
     this.signupRequestPayload.password = this.signupForm.get('password').value;
 
     this.authService.signup(this.signupRequestPayload)
-    .subscribe(data => {
-      console.log(data);
+    .subscribe(() => {
+      this.router.navigate(['/login'],
+       { queryParams: {registered: 'true'}});
+    }, () => {
+      this.toastr.error('Registration Failed! Please try again');
     })
-
-    // this.authService.signup(this.signupRequestPayload)
-    // .subscribe(() => {
-    //   this.router.navigate(['/login'],
-    //    { queryParams: {registered: 'true'}});
-    // }, () => {
-    //   this.toastr.error('Registration Failed! Please try again');
-    // })
   }
 }
